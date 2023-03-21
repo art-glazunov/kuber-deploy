04ffb493d786fd822eb49123242abc12dcf3b79e
Артём Глазунов <artemglazunov@MacBook-Pro-Artem.local>
2023-03-21 13:32:25 +0300

    add peak load test

--- load_test_peak.js
+++ load_test_peak.js
@@ -1,20 +1,14 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
-export let options = {
-    stages: [
-        { duration: '5s', target: 50 }, 
-        { duration: '5s', target: 100}, 
-        { duration: '5s', target: 300 }, 
-        { duration: '20s', target: 600 }, 
-        { duration: '5s', target: 300}, 
-        { duration: '5s', target: 200 }, 
-        { duration: '10s', target: 0 }, 
-      ],
-};
+export const options = {
+	  vus: 150,
+	  duration: '10s',
+	};
+
 
 export default function () {
-    group('main API check', () => {
+    group('API check', () => {
         const response = http.get('http://localhost:4000');
         check(response, {
             "status code should be 200": res => res.status === 200,
@@ -22,7 +16,7 @@ export default function () {
         sleep(4);
     });
     group('Check neg response', () => {
-        const response = http.post('http://localhost:4000/',
+        const response = http.post('http://localhost:4000',
                 {
                     "text": "ужасно"
                 }
@@ -36,7 +30,7 @@ export default function () {
         sleep(4);
     });
     group('Check pos response', () => {
-        const response = http.post('http://localhost:4000/',
+        const response = http.post('http://localhost:4000',
                 {
                     "text": "отлично"
                 }
